26cc83980452cf6bb36a333fc02c5c05dccb759e
Konner Bratland <116665821+Kbratland@users.noreply.github.com>
2025-02-25 18:14:59 -0800

    e

--- src/main/java/frc/robot/subsystems/coralSuperComponentSubsystem.java
+++ src/main/java/frc/robot/subsystems/coralSuperComponentSubsystem.java
@@ -1,11 +1,10 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
+import ctre
 public class coralSuperComponentSubsystem extends SubsystemBase {
     
-    private final TalonSRX
+    
 
 
 }
