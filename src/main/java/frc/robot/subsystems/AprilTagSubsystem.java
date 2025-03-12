bda1ab53a96f0c08f233d6c3cff3a4e7625ffb3b
BjornBlog <116665790+BjornBlog@users.noreply.github.com>
2025-03-12 14:42:36 -0700

    Create AprilTagSubsystem.java

---
+++ src/main/java/frc/robot/subsystems/AprilTagSubsystem.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems;
+
+import java.util.Vector;
+
+public class AprilTagSubsystem {
+    private static double posX[];
+    private static double posY[];
+    private static double angle[];
+    Vector rePosition(int tagID){
+        
+        return 0;
+    }
+}
